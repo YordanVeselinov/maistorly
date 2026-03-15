498f8dc78d0da5f42880f4ab00367a4fd0bae0db
Yordan Veselinov <yordan.r.veselinov@gmail.com>
2026-03-15 13:14:14 +0200

    feat: add customer account profile edit flow and stabilize auth flow with end-to-end account tests

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 
-from .forms import RegisterForm, EmailAuthenticationForm
+from .forms import CustomerAccountForm, EmailAuthenticationForm, RegisterForm
+from .models import CustomerAccount
 from .signals import CLIENTS_GROUP
 
 
@@ -22,11 +23,31 @@ class RegisterView(CreateView):
         user = form.save()
         client_group, _ = Group.objects.get_or_create(name=CLIENTS_GROUP)
         user.groups.add(client_group)
+        CustomerAccount.objects.get_or_create(user=user)
         return redirect(self.get_success_url())
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = "accounts/profile.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        customer_account, _ = CustomerAccount.objects.get_or_create(user=self.request.user)
+        context["customer_account"] = customer_account
+        context["primary_group"] = self.request.user.groups.order_by("name").first()
+        return context
+
+
+class ProfileEditView(LoginRequiredMixin, UpdateView):
+    form_class = CustomerAccountForm
+    template_name = "accounts/profile_edit.html"
+    success_url = reverse_lazy("accounts:profile")
+
+    def get_object(self, queryset=None):
+        customer_account, _ = CustomerAccount.objects.get_or_create(user=self.request.user)
+        return customer_account
+
+
 
 
 class AccountLoginView(LoginView):
